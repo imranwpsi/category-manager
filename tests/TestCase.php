443fd621bae28bee99b7c3558f0c927f19a400dc
imranwpsi <29890208+imranwpsi@users.noreply.github.com>
2025-05-11 10:47:01 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace Imranwpsi\CategoryManager\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Imranwpsi\CategoryManager\CategoryManagerServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
